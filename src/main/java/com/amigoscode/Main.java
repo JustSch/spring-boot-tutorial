b332e226235128adf4955847ebfbb47a76848f66
Justin Schreiber <jschreiber234@gmail.com>
2023-02-08 17:40:39 -0500

    creating basic customer api

--- src/main/java/com/amigoscode/Main.java
+++ src/main/java/com/amigoscode/Main.java
@@ -3,29 +3,27 @@ package com.amigoscode;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @SpringBootApplication
 @RestController
+@RequestMapping("api/v1/customers")
 public class Main {
+    private final CustomerRepository customerRepository;
+
+    public Main(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
     }
-    @GetMapping("/greet")
-    public GreetResponse greet(){
-        GreetResponse response = new GreetResponse("Hello",
-                List.of("Java","Golang","Javascript"),
-                new Person("Alex",28,30_000));
-        return response;
-    }
 
-    record Person (String name, int age, double savings) {
-
+    @GetMapping
+    public List<Customer> getCustomers(){
+        return customerRepository.findAll();
     }
-    record GreetResponse(String  greet,
-                         List<String> favProgrammingLanguages,
-                         Person Person
-                         ){}
 }
